4613c3721bdda9108973a5e2dad76be87da0d127
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-19 03:17:30 +0530

    Create 5. LeftRotateOnePlace.cpp

---
+++ Arrays/5. LeftRotateOnePlace.cpp
@@ -0,0 +1,13 @@
+// Optimal Code
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int temp = arr [0];
+    for (int i=1; i<n; i++){
+        arr[i-1]=arr[i];
+    }
+    arr[n-1] = temp ;
+    return arr;
+}
