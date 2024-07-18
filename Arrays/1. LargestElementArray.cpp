4d77eb2fd1282996fc6ed9bc38ea6f45cb06ab30
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-19 01:57:53 +0530

    Update 1. LargestElementArray.cpp

--- Arrays/1. LargestElementArray.cpp
+++ Arrays/1. LargestElementArray.cpp
@@ -8,3 +8,18 @@ int main() {
     std::cout << "The largest element in the array is: " << largest << std::endl;
     return 0;
 }
+
+// Optimal Code
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int arr[] = { 3, 7, 1, 9, 5, 15 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int largest = arr[0];
+    for (int i = 0; i<n ; i++){
+        if (arr[i] > largest){
+            largest = arr [i];
+        }
+    }
+    cout << "The largest number in the array is: " << largest;
+}
