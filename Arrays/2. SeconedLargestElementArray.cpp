4fb340a2f3583c7f2a10fb391f4db7d688fe2d2e
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-19 02:18:17 +0530

    Update 2. SeconedLargestElementArray.cpp

--- Arrays/2. SeconedLargestElementArray.cpp
+++ Arrays/2. SeconedLargestElementArray.cpp
@@ -17,3 +17,25 @@ int main(){
 }
 
 // Better Solution
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int arr[] = { 3, 7, 1, 9, 5, 15, 15 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int largest = arr[0];
+    int slargest = -1 ;
+    for (int i = 0; i<n ; i++){
+        if (arr[i] > largest){
+            largest = arr [i];
+        }
+    }
+    cout << "The largest number in the array is: " << largest << "\n";
+    for (int i = 0; i<n ; i++){
+        if ((arr[i] != largest) && (arr[i]>slargest)){
+            slargest = arr [i];
+        }
+    }
+    cout << "The seconed largest number in the array is: " << slargest;
+}
+
+// Optimal Solution
