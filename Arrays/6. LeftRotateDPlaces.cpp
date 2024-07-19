fc3faef6e926bf96780be85e664fdbc86ec930de
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-19 19:12:26 +0530

    Update 6. LeftRotateDPlaces.cpp

--- Arrays/6. LeftRotateDPlaces.cpp
+++ Arrays/6. LeftRotateDPlaces.cpp
@@ -1,1 +1,21 @@
 // Brute Force
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int d = 3;
+    int temp[d-1]={};
+    for (int i=0; i<d;i++){
+        temp[i] = arr[i];
+    }
+    for (int i=d;i<n;i++){
+        arr[i-d] = arr[i];
+    }
+    for (int i=n-d; i<n;i++){
+        arr[i] = temp[i-(n-d)];
+    }
+    cout<<arr[0]<<arr[1];
+}
+
+// Optimal Solution
