af775a6103518efa33e4cf7851bb8e7fb5fc89c1
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-20 02:36:51 +0530

    Update 9. UnionSortedArray.cpp

--- Arrays/9. UnionSortedArray.cpp
+++ Arrays/9. UnionSortedArray.cpp
@@ -1,1 +1,23 @@
 // Brute Force
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int arr1[] = {1,2,3,0,0,4,0,5,0,6,7};
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    int arr2[] = {1,2,3,4,4,5,6,7,8,9,44};
+    int n2 = sizeof(arr2)/sizeof(arr2[0]);
+    set <int> st;
+    for (int i=0;i< n1;i++){
+        st.insert(arr1[i]);
+    }
+    for (int i=0;i< n2;i++){
+        st.insert(arr2[i]);
+    }
+    std::vector<int> temp;
+    for(auto it:st){
+        temp.push_back(it);
+    }
+    return temp;
+}
+
+// Optimal Solution
