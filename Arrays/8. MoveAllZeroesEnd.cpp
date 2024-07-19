2d42b4e360c674a430ad212b1e8c84d7979d9e38
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-19 23:50:15 +0530

    Update 8. MoveAllZeroesEnd.cpp

--- Arrays/8. MoveAllZeroesEnd.cpp
+++ Arrays/8. MoveAllZeroesEnd.cpp
@@ -1,1 +1,41 @@
 // Brute Force
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int arr[] = {1,2,3,0,0,4,0,5,0,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector <int> temp;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            temp.push_back(arr[i]);
+        }
+    }
+    int nz=temp.size();
+    for(int i=0;i<nz;i++){
+        arr[i]=temp[i];
+    }
+    for(int i=nz;i<n;i++){
+        arr[i]=0;
+    }
+}
+
+// Optimal Solution
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int arr[] = {1,2,3,0,0,4,0,5,0,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int j= -1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            j=i;
+            break;
+        }
+    }
+    for(int i=(j+1);i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+}
