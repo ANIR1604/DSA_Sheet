f73b2c31b725019595bf5429116663633b549474
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-19 21:05:25 +0530

    Update 6. LeftRotateDPlaces.cpp

--- Arrays/6. LeftRotateDPlaces.cpp
+++ Arrays/6. LeftRotateDPlaces.cpp
@@ -19,3 +19,14 @@ int main() {
 }
 
 // Optimal Solution
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int d = 3;
+    cout << endl;
+    reverse(arr,arr+d);
+    reverse(arr+d,arr+n);
+    reverse(arr,arr+n);
+}
