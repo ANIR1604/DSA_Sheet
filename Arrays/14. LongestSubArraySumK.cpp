e9fea913168dea5d105e2c69c594126f7d2b84e5
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-21 00:49:10 +0530

    Update 14. LongestSubArraySumK.cpp

--- Arrays/14. LongestSubArraySumK.cpp
+++ Arrays/14. LongestSubArraySumK.cpp
@@ -86,3 +86,34 @@ int main()
 }
 
 // Optimal Solution (Two Pointer)
+#include <bits/stdc++.h>
+using namespace std;
+int getLongestSubarray(vector<int>& a, long long k) {
+  int left=0;
+  int right=0;
+  long long sum = a[0];
+  int maxlen = 0;
+  int n = a.size();
+  while (right<n){
+    while(left<=right && sum>k){
+      sum-=a[left];
+      left++;
+    }
+    if(sum==k){
+      maxlen = max(maxlen,right-left+1);
+    }
+    right ++;
+    if(right<n){
+      sum+=a[right];
+    }
+  }
+  return maxlen;
+}
+int main()
+{
+    vector<int> a = {2, 3, 5, 1, 9};
+    long long k = 10;
+    int len = getLongestSubarray(a, k);
+    cout << "The length of the longest subarray is: " << len << "\n";
+    return 0;
+}
