53dcaa131d3f11a5109e816f59335f1dbd98f5f2
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-20 14:14:53 +0530

    Update 12. MissingNumber.cpp

--- Arrays/12. MissingNumber.cpp
+++ Arrays/12. MissingNumber.cpp
@@ -48,4 +48,46 @@ int main()
     return 0;
 }
 
-// Optimal Solution
+// Optimal Solution (Arithmetic)
+#include <bits/stdc++.h>
+using namespace std;
+int missingNumber(vector<int>&a, int N) {
+  int sum = (N*(N+1))/2;
+  int sum1=0;
+  for (int i=0;i<a.size();i++){
+    sum1 += a[i];
+  }
+  return (sum-sum1);
+}
+int main()
+{
+    int N = 5;
+    vector<int> a = {1, 2, 4, 5};
+    int ans = missingNumber(a, N);
+    cout << "The missing number is: " << ans << endl;
+    return 0;
+}
+
+// Optimal Solution (XOR)
+#include <bits/stdc++.h>
+using namespace std;
+int missingNumber(vector<int>&a, int N) {
+  int xor1=0;
+  int xor2=0;
+  int n=N-1;
+  for(int i=0;i<n;i++){
+    xor1 = xor1 ^ (i+1);
+    xor2 = xor2 ^ a[i];
+  }
+  xor1 = xor1 ^ N;
+  return xor1^xor2;
+}
+int main()
+{
+    int N = 5;
+    vector<int> a = {1, 2, 4, 5};
+    int ans = missingNumber(a, N);
+    cout << "The missing number is: " << ans << endl;
+    return 0;
+}
+
