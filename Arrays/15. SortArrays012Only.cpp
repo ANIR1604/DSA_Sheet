0c3190d8b6374f7371c9a880e63b228cefec4530
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-23 17:41:31 +0530

    Update 15. SortArrays012Only.cpp

--- Arrays/15. SortArrays012Only.cpp
+++ Arrays/15. SortArrays012Only.cpp
@@ -26,3 +26,34 @@ int main()
 }
 
 // Optimal Solution (DUTCH NATIONAL FLAG Algorithm)
+#include <bits/stdc++.h>
+using namespace std;
+void sortArray(vector<int>& a, int n) {
+    int low = 0;
+    int mid = 0;
+    int high = n-1;
+    while (mid<=high){
+      if (a[mid]==0){
+        swap(a[mid],a[low]);
+        low++;
+        mid++;
+      }else if(a[mid]==1){
+        mid++;
+      }else{
+        swap(a[mid],a[high]);
+        high--;
+      }
+    }
+}
+int main()
+{
+    int n = 6;
+    vector<int> arr = {0, 2, 1, 2, 0, 1};
+    sortArray(arr, n);
+    cout << "After sorting:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
