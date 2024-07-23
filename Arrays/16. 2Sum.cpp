948718641198865a5a7472050d3af5d21aa3fcd8
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-24 01:39:51 +0530

    Update 16. 2Sum.cpp

--- Arrays/16. 2Sum.cpp
+++ Arrays/16. 2Sum.cpp
@@ -20,3 +20,28 @@ int main()
 }
 
 // Better Solution
+#include <bits/stdc++.h>
+using namespace std;
+string twoSum(int n, vector<int> &arr, int target) {
+    map <int,int> mpp;
+    for (int i=0; i<n;i++){
+      int num = arr[i];
+      int moreneeded = target-num;
+      if(mpp.find(moreneeded) != mpp.end()){
+        return "YES";
+      }
+      mpp[num] = i;
+    }
+    return "NO";
+}
+int main()
+{
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    string ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant : " << ans << endl;
+    return 0;
+}
+
+// Optimal Solution (Two Pointer Approach)
