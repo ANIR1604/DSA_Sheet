5ccf0bce37193c492b39c30271d976a5c599fd1d
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-24 13:39:40 +0530

    Update 17. MajorityElements.cpp

--- Arrays/17. MajorityElements.cpp
+++ Arrays/17. MajorityElements.cpp
@@ -26,4 +26,27 @@ int main()
 }
 
 // Better Approach
+#include <bits/stdc++.h>
+using namespace std;
+int majorityElement(vector<int> v) {
+  map <int,int> mpp;
+  int n = v.size();
+  for (int i=0;i<n;i++){
+    mpp[v[i]]++;
+  }
+  for (auto it: mpp){
+    if (it.second > n/2){
+      return it.first;
+    }
+  }
+  return -1;
+}
+int main()
+{6
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+    int ans = majorityElement(arr);
+    cout << "The majority element is: " << ans << endl;
+    return 0;
+}
 
+// Optimal Solution 
