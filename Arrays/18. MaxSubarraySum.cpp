f4702e0508a6681d6eb1d418f71a29da82a30994
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-25 13:37:47 +0530

    Update 18. MaxSubarraySum.cpp

--- Arrays/18. MaxSubarraySum.cpp
+++ Arrays/18. MaxSubarraySum.cpp
@@ -47,3 +47,27 @@ int main()
 }
 
 // Optimal Solution (KADANE Algorithm)
+#include <bits/stdc++.h>
+using namespace std;
+int maxSubarraySum(int arr[], int n) {
+    int maxi=INT_MIN;
+    long long sum = 0;
+    for (int i=0;i<n;i++){
+      sum+=arr[i];
+      if(sum>maxi){
+        maxi = sum;
+      }
+      if(sum<0){
+        sum=0;
+      }
+    }
+    return maxi;
+}
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
