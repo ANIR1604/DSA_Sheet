b604070444cf874b7ef53479c8ea2de8179f41fb
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-25 10:39:38 +0530

    Update 17. MajorityElements.cpp

--- Arrays/17. MajorityElements.cpp
+++ Arrays/17. MajorityElements.cpp
@@ -50,3 +50,35 @@ int main()
 }
 
 // Optimal Solution (MOORE'S VOTING Algorithm)
+#include <bits/stdc++.h>
+using namespace std;
+int majorityElement(vector<int> v) {
+  int elem , cnt=0 , cnt1=0; 
+  int n = v.size();
+  for (int i=0;i<n;i++){
+    if (cnt == 0){
+      cnt = 1;
+      elem=v[i];
+    }else if(v[i]==elem){
+      cnt++;
+    }else{
+      cnt--;
+    }
+  }
+  for(int i=0;i<n;i++){
+    if(v[i]=elem){
+      cnt1++;
+    }
+    if(cnt1>n/2){
+      return elem;
+    }
+  }
+  return -1;
+}
+int main()
+{
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+    int ans = majorityElement(arr);
+    cout << "The majority element is: " << ans << endl;
+    return 0;
+}
