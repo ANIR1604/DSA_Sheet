57fdd7f15ac663aaeb68d61e2e0a63ca2d094581
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-31 12:32:59 +0530

    Update 19. RearrangeBySign.cpp

--- Arrays/19. RearrangeBySign.cpp
+++ Arrays/19. RearrangeBySign.cpp
@@ -30,3 +30,31 @@ int main()
 }
 
 // Better Solution
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> rearrangeArr(vector<int> arr, int n) {
+    std::vector<int> ans(n,0);
+    int pos=0, neg=1;
+    for(int i=0;i<n;i++){
+      if(arr[i]<0){
+        ans[neg]=arr[i];
+        neg+=2;
+      }else{
+        ans[pos]=arr[i];
+        pos+=2;
+      }
+    }
+    return ans;
+}
+int main()
+{
+    std::vector<int> arr{3,-2,1,-5,2,-4};
+    int n = arr.size();
+    std::vector<int> newArr = rearrangeArr(arr, n);
+    for(int i=0;i<newArr.size();i++){
+      cout<<newArr[i]<<" ";
+    }
+    return 0;
+}
+
+// Optimal Approach
