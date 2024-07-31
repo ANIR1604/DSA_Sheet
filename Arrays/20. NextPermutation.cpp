6cc4ec4db8b7256aa8df33d2ff75dd28ad131df3
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-31 15:56:44 +0530

    Update 20. NextPermutation.cpp

--- Arrays/20. NextPermutation.cpp
+++ Arrays/20. NextPermutation.cpp
@@ -9,3 +9,38 @@ int main() {
 }
 
 // Optimal Approach
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> nextGreaterPermutation(vector<int> &A) {
+  int index = -1;
+  int n = A.size();
+  for(int i=n-2;i>=0;i--){
+    if(A[i]<A[i+1]){
+      index = i;
+      break;
+    }
+  }
+  if(index==-1){
+    reverse(A.begin(),A.end());
+    return A;
+  }
+  for(int i=n-1; i>index;i--){
+    if(A[i]>A[index]){
+      swap(A[i],A[index]);
+      break;
+    }
+  }
+  reverse(A.begin()+index+1,A.end());
+  return A;
+}
+int main()
+{
+    vector<int> A = {2, 1, 5, 4, 3, 0, 0};
+    vector<int> ans = nextGreaterPermutation(A);
+    cout << "The next permutation is: [ ";
+    for (auto it : ans) {
+        cout << it << " ";
+    }
+    cout << "]";
+    return 0;
+}
