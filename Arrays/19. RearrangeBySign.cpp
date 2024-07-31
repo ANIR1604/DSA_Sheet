018687619032deec4364ea157efb2bfd49e98e9c
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-07-31 11:54:14 +0530

    Update 19. RearrangeBySign.cpp

--- Arrays/19. RearrangeBySign.cpp
+++ Arrays/19. RearrangeBySign.cpp
@@ -1,1 +1,32 @@
 // Brute Force
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> rearrangeArr(vector<int> arr, int n) {
+    std::vector<int> pos;
+    std::vector<int> neg;
+    for (int i=0;i<n;i++){
+      if(arr[i]<0){
+        neg.push_back(arr[i]);
+      }else{
+        pos.push_back(arr[i]);
+      }
+    }
+    std::vector<int> v;
+    for(int i=0;i<n/2;i++){
+      arr[2*i]=pos[i];
+      arr[2*i + 1] = neg[i];
+    }
+    return arr;
+}
+int main()
+{
+    std::vector<int> arr{3,-2,1,-5,2,-4};
+    int n = arr.size();
+    std::vector<int> newArr = rearrangeArr(arr, n);
+    for(int i=0;i<newArr.size();i++){
+      cout<<newArr[i]<<" ";
+    }
+    return 0;
+}
+
+// Better Solution
