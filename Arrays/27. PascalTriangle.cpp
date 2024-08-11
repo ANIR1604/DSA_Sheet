e00bbbc3278e146e088468894d2829f80019629e
Anirban Biswas <139000437+ANIR1604@users.noreply.github.com>
2024-08-11 17:44:08 +0530

    Update 27. PascalTriangle.cpp

--- Arrays/27. PascalTriangle.cpp
+++ Arrays/27. PascalTriangle.cpp
@@ -1,1 +1,118 @@
-// Variation I
+// Variation I (Print the number at particular row and column)
+#include <bits/stdc++.h>
+using namespace std;
+int nCr(int n, int r) {
+    long long res = 1;
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+int pascalTriangle(int r, int c) {
+    int element = nCr(r - 1, c - 1);
+    return element;
+}
+int main()
+{
+    int r = 5;
+    int c = 3; 
+    int element = pascalTriangle(r, c);
+    cout << "The element at position (r,c) is: "
+            << element;
+    return 0;
+}
+        
+// Variation II (Print whole row)
+#include <bits/stdc++.h>
+using namespace std;
+void pascalTriangle(int n) {
+    long long ans = 1;
+    cout << ans << " ";
+    for (int i = 1; i < n; i++) {
+        ans = ans * (n - i);
+        ans = ans / i;
+        cout << ans << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    int n = 5;
+    pascalTriangle(n);
+    return 0;
+}
+
+// Variation III (Generate all rows - Brute Force)
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+
+vector<vector<int>> pascalTriangle(int n) {
+    vector<vector<int>> ans;
+    for (int row = 1; row <= n; row++) {
+        vector<int> tempLst; // temporary list
+        for (int col = 1; col <= row; col++) {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 5;
+    vector<vector<int>> ans = pascalTriangle(n);
+    for (auto it : ans) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// Variation III (Generate all rows - Optimal Solution)
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> generateRow(int row) {
+    long long ans = 1;
+    vector<int> ansRow;
+    ansRow.push_back(1); 
+    for (int col = 1; col < row; col++) {
+        ans = ans * (row - col);
+        ans = ans / col;
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+vector<vector<int>> pascalTriangle(int n) {
+    vector<vector<int>> ans;
+    for (int row = 1; row <= n; row++) {
+        ans.push_back(generateRow(row));
+    }
+    return ans;
+}
+int main()
+{
+    int n = 5;
+    vector<vector<int>> ans = pascalTriangle(n);
+    for (auto it : ans) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
